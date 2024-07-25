3afd53852d8653b4f90eaaa269971b8af94dc97d
Siwei Du <seaweedgo@gmail.com>
2024-07-25 19:08:17 -0400

    clean up

--- data_collection/main.py
+++ data_collection/main.py
@@ -5,18 +5,17 @@ from time import sleep
 
 def read_serial():
     print("Test")
-    with serial.Serial('/dev/cu.SLAB_USBtoUART', 115200, timeout=1) as ser:
+    with serial.Serial('/dev/cu.SLAB_USBtoUART', 9600, timeout=2) as ser:
         while ser.is_open:
             line = ser.readline() # will need to be decoded
             update_label(line.decode('utf-8'))
-            sleep(0.0050)
+            # sleep(0.0050)
 
 def update_label(data):
     lbl.config(text=data)
 
-def submit():
-    dTime = delayTime.get()
-
+def startRecord():
+    print("RECORDING STARTED\n")
 
 root = tk.Tk()
 root.geometry("600x400")
@@ -24,21 +23,19 @@ root.title("Live Serial Data")
 
 lbl = tk.Label(root, text="Waiting for data...", font=("Helvetica", 16))
 #lbl.pack(padx=20,pady=20)
-lbl.grid(row=0,column=1);
+lbl.grid(row=0,column=1)
 
 delayTime = tk.IntVar()
 delayTimeEntry = tk.Entry(root, textvariable = delayTime, font=("Helvetica", 16))
-sub_btn = tk.Button(root, text = 'Submit',command = submit)
-
+rec_btn = tk.Button(root, text = 'Record',command = startRecord)
 
 delayTimeEntry.grid(row=1,column=1)
-sub_btn.grid(row=2,column=1)
+rec_btn.grid(row=2,column=1)
 
 
-sleep(1);
+
+# =============================================================================
+sleep(1)
 thread = threading.Thread(target=read_serial).start()
+root.mainloop()
 
-
-
-
-root.mainloop();
---
+++ data_collection/notes.md
@@ -0,0 +1,12 @@
+Design Notes
+
+Features:
+1. Record varying length windows of data (time dependent for now)
+    - Field to enter length
+    - Field to select units'
+2. Recording:
+    - Horizontal progress bar, with a 1 or 0.5s ready phase, and then the X second recording phase, and a 1 or 0.5s end phase
+        - This way, user can record accurately
+    - Interval recording: Record Y sets
+        - Field to enter Y samples
+    - Free recording: Record arbitrary amount of time.   <== current focus
