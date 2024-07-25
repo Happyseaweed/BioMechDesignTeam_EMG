7587eca77f69b2a3c1eb5deaa1c17bf9fdd80549
Siwei Du <seaweedgo@gmail.com>
2024-07-25 19:46:52 -0400

    added basic recording capabilities, saves recording to a folder ./saves/

--- data_collection/main.py
+++ data_collection/main.py
@@ -1,21 +1,51 @@
 import serial
 import threading
 import tkinter as tk
+import datetime
+import numpy as np
+import os
 from time import sleep
+
+# Constants
+# TODO: probably make this in a class so we can do OOP
+
+recordingStarted = False
+recordedData = []
 
 def read_serial():
     print("Test")
+    global recordingStarted
+    global recordedData
     with serial.Serial('/dev/cu.SLAB_USBtoUART', 9600, timeout=2) as ser:
         while ser.is_open:
             line = ser.readline() # will need to be decoded
-            update_label(line.decode('utf-8'))
+            decoded = line.decode('utf-8')
+            update_label(decoded)
             # sleep(0.0050)
+            if recordingStarted:
+                decoded = decoded[:-2]
+                recordedData.append(decoded) 
 
 def update_label(data):
     lbl.config(text=data)
 
-def startRecord():
-    print("RECORDING STARTED\n")
+def toggleRecord():
+    global recordingStarted
+    global recordedData
+    if (not recordingStarted): 
+        rec_btn.config(text="Stop")
+        print("RECORDING [STARTED]\n")
+    else: 
+        rec_btn.config(text="Start")
+        print("RECORDING [STOPPED]\n")
+        saveData = np.array(recordedData, dtype=np.int16)
+        print(saveData)
+        print(saveData.shape)
+        if not os.path.exists("saves/"):
+            os.makedirs("saves")
+        np.savetxt(f"saves/{datetime.datetime.now()}_Trail_0.csv", saveData, fmt="%d")
+        recordedData = []
+    recordingStarted = not recordingStarted
 
 root = tk.Tk()
 root.geometry("600x400")
@@ -27,12 +57,10 @@ lbl.grid(row=0,column=1)
 
 delayTime = tk.IntVar()
 delayTimeEntry = tk.Entry(root, textvariable = delayTime, font=("Helvetica", 16))
-rec_btn = tk.Button(root, text = 'Record',command = startRecord)
+rec_btn = tk.Button(root, text = 'Start' , command = toggleRecord)
 
 delayTimeEntry.grid(row=1,column=1)
 rec_btn.grid(row=2,column=1)
-
-
 
 # =============================================================================
 sleep(1)
