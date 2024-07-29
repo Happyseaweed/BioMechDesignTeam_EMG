8b1d0610aa9947ecf6f16ecf83897cf9faf41ba0
w12l3-c <wallace.lee0412@gmail.com>
2024-07-29 02:23:31 -0400

    Add offline mode

--- data_collection/main.py
+++ data_collection/main.py
@@ -1,9 +1,11 @@
-import serial
+import serial   # pip install pyserial, not serial
 import threading
 import tkinter as tk
 import datetime
 import numpy as np
 import os
+import random
+import time
 from time import sleep
 
 # Constants
@@ -16,15 +18,36 @@ def read_serial():
     print("Test")
     global recordingStarted
     global recordedData
-    with serial.Serial('/dev/cu.SLAB_USBtoUART', 9600, timeout=2) as ser:
-        while ser.is_open:
-            line = ser.readline() # will need to be decoded
-            decoded = line.decode('utf-8')
-            update_label(decoded)
-            # sleep(0.0050)
-            if recordingStarted:
-                decoded = decoded[:-2]
-                recordedData.append(decoded) 
+    try:
+        with serial.Serial('/dev/cu.SLAB_USBtoUART', 9600, timeout=2) as ser:
+            while ser.is_open:
+                line = ser.readline() # will need to be decoded
+                decoded = line.decode('utf-8')
+                update_label(decoded)
+                # sleep(0.0050)
+                if recordingStarted:
+                    decoded = decoded[:-2]
+                    recordedData.append(decoded) 
+    # When you don't have esp32 connected but just work on the GUI itself
+    except Exception as e:
+        print("Currently at offline mode, generating random data")
+        # Send fake data with random pockets of EMG
+        while True:
+            start_time = time.time()
+            while time.time()-start_time < 5:
+                decoded = random.randint(0, 100)
+                time.sleep(0.01)
+                update_label(decoded)
+                if recordingStarted:
+                    recordedData.append(decoded)
+            start_time = time.time()
+            while time.time()-start_time < 2.5:
+                decoded = random.randint(1000, 2000)
+                time.sleep(0.01)
+                update_label(decoded)
+                if recordingStarted:
+                    recordedData.append(decoded)
+
 
 def update_label(data):
     lbl.config(text=data)
