d54a159cf67794a7f38e483eb9e00912db71dac0
Siwei Du <seaweedgo@gmail.com>
2024-10-01 19:38:04 -0400

    Centered stuff, minor change

--- data_collection/main.py
+++ data_collection/main.py
@@ -7,6 +7,9 @@ import os
 import random
 import time
 from time import sleep
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.animation as animation
 
 # Constants
 # TODO: probably make this in a class so we can do OOP
@@ -75,18 +78,24 @@ root.geometry("600x400")
 root.title("Live Serial Data")
 
 lbl = tk.Label(root, text="Waiting for data...", font=("Helvetica", 16))
-#lbl.pack(padx=20,pady=20)
-lbl.grid(row=0,column=1)
+lbl.grid(row=0, column=0, columnspan=3, pady=20)
 
 delayTime = tk.IntVar()
 delayTimeEntry = tk.Entry(root, textvariable = delayTime, font=("Helvetica", 16))
+delayTimeEntry.grid(row=1, column=1, padx=20, pady=10)
+
 rec_btn = tk.Button(root, text = 'Start' , command = toggleRecord)
+rec_btn.grid(row=2, column=1, padx=20, pady=10)
 
 delayTimeEntry.grid(row=1,column=1)
 rec_btn.grid(row=2,column=1)
+
+# Make sure everything is centered in the window
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=1)
 
 # =============================================================================
 sleep(1)
 thread = threading.Thread(target=read_serial).start()
 root.mainloop()
-
